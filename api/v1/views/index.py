0cd8ba55cfecf74207737ec5d3b9587aa4de94f3
hacheG <943@holbertonschool.com>
2020-01-27 19:08:32 -0500

    [FEATURE] task_5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,19 @@
 """List of states"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
 
 @app_views.route('/status', strict_slashes=False)
 def api_status():
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def some_stats():
+    return jsonify({'amenities': storage.count('Amenity'),
+                   'cities': storage.count('City'),
+                   'places': storage.count('Place'),
+                   'reviews': storage.count('Review'),
+                   'states': storage.count('State'),
+                   'users': staorage.count('User')})
