198f3bd08298ff607889a4a6ce15a993cd89dd05
julgachancipa <julianaga01@gmail.com>
2020-01-28 14:59:12 +0000

    [FEATURE] - not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,7 +18,8 @@ def close_s(x=None):
 @app.errorhandler(404)
 def not_found_error(error):
     """oh my cat"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+    error = jsonify({"error": "Not found"})
+    return error
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=True)
