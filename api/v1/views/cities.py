f0704ff211412310dd8dc6d86f4a9547feae0c7f
julgachancipa <julianaga01@gmail.com>
2020-01-29 16:14:07 +0000

    [FEATURE] State API

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-"""cities api"""
+"""States api"""
+from api.v1.views import app_views
+from flask import Flask, jsonify, request, abort
 from models import storage
 from models.city import City
-from flask import Flask, jsonify, request, abort
-from api.v1.views import app_views
 
 
+@app_views.route('/states/<state_id>/cities', strict_slashes=False)
+def state_cities(state_id):
+    """find a state's cities"""
+    state = storage.get('State', state_id)
+    if state is None:
+        abort(404)
+    print('>>>', state)
+    return jsonify({'oli': 'olis'})
