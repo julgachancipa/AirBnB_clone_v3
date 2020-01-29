f2f84beaabf21a18090542fda884eccc65cfc28d
hacheG <943@holbertonschool.com>
2020-01-29 14:45:14 -0500

    [FIX] change functions names

--- api/v1/views/place.py
+++ api/v1/views/place.py
@@ -5,19 +5,24 @@ from flask import Flask, jsonify, request, abort
 from models import storage
 from models.place import Place
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
-def all_users():
-    """return all places"""
-    users_dict = storage.all('State')
-    users_list = list(users_dict.values())
-    json_list = []
-    for u in users_list:
-        json_list.append(u.to_dict())
-    return jsonify(json_list)
+@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+def create_place(state_id):
+    """creates a new state"""
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    if not 'name' in request.get_json():
+        abort(400, 'Missing name')
+    state = storage.get('State', state_id)
+    if state is None:
+        abort(404)
+    nw_city = City(name=request.json['name'], state_id=state_id)
+    storage.new(nw_city)
+    storage.save()
+    return jsonify(nw_city.to_dict()), 201
 
 
 @app_views.route('/users/<user_id>', strict_slashes=False, methods=['GET'])
-def user_id(user_id):
+def place_id(user_id):
     """find a user by id"""
     users = storage.get('User', user_id)
     if users is not None:
@@ -27,7 +32,7 @@ def user_id(user_id):
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
-def del_user(user_id):
+def del_place(user_id):
     """delete user id"""
     users = storage.get('User', user_id)
     if users is None:
@@ -38,7 +43,7 @@ def del_user(user_id):
 
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
-def create_users():
+def create_places():
     """creates a new users"""
     if not request.get_json():
         abort(400, 'Not a JSON')
@@ -51,7 +56,7 @@ def create_users():
 
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
-def update_users(user_id):
+def update_places(user_id):
     """update_task"""
     users = storage.get('User', user_id)
     if users is None:
