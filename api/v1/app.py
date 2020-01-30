b2eb712a799055841a1c91a21837b7b170c7b021
hacheG <943@holbertonschool.com>
2020-01-30 09:34:40 -0500

    [FEATURE] add task_13 CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,17 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+#cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+#NO SE CUAL DE LAS DOS SEA LA APROPIADA, LA PRIMERA
+#ESPECIFICA LA RUTA DESDE api.. PERO LA SEGUNDA CUBRE
+#TODOS LOS ARCHIVOS, ASI QUE TOCA PREGUNTAR,
+#LO OTRO ES QUE YO LA PUSE AL FINAL, PERO PUEDE
+#QUE TENGA OTRA UBICACION
 
 
 @app.teardown_appcontext
