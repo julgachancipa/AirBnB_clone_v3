5e0dd08f2d1d01baf54ef4a136ce37d45810d4b2
julgachancipa <julianaga01@gmail.com>
2020-01-30 01:30:04 +0000

    [FEATURE] order

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 Status of your API
 """
-from flask import Flask
+from flask import Flask, Blueprint
 from models import storage
 from api.v1.views import app_views
 from flask import jsonify
@@ -12,20 +12,20 @@ app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
-@app.teardown_appcontext
-def close_s(x=None):
-    """
-    close session at the end
-    """
-    storage.close()
-
-
 @app.errorhandler(404)
 def not_found_error(error):
     """
     oh my cat
     """
     return jsonify(error="Not Found"), 404
+
+
+@app.teardown_appcontext
+def close_s(x=None):
+    """
+    close session at the end
+    """
+    storage.close()
 
 
 if __name__ == '__main__':
