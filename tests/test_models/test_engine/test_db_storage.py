ca0a80eff46c54a311fa8743af91f630180bf56b
hacheG <943@holbertonschool.com>
2020-01-30 14:41:38 -0500

    [TEST] variables

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -90,7 +90,7 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_get(self):
         """tests get method"""
-        newStatecall_stat = State(name="Texas")
+        newStatecall_sta = State(name="Texas")
         models.storage.new(newStatecall_state)
         models.storage.save()
         obj = models.storage.get('State', newStatecall_state.id)
